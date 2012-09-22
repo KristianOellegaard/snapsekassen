387524c3f01c1aebc8f2c799d85a00fd2906589c
Kristian Øllegaard <kristian@kristian.io>
2012-09-22 21:58:40 +0200

    include scnappsaccount in urls

--- project/urls.py
+++ project/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^project/', include('project.foo.urls')),
+    url(r'^', include('schnappsaccount.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
