985bfcd8ec5366c0b3ecc7ba18b556335a4e85c9
Kristian Øllegaard <kristian@kristian.io>
2012-09-24 16:53:59 +0200

    Also add reason to the list

--- schnappsaccount/admin.py
+++ schnappsaccount/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import Person, Claim
 
 class ClaimAdmin(admin.ModelAdmin):
-    list_display = ('person', 'reported_by', 'date')
+    list_display = ('person', 'reported_by', 'date', 'reason')
 
 admin.site.register(Person)
 admin.site.register(Claim, ClaimAdmin)
